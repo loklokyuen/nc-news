ded60b40d433780f0a4948892a437da2171bb8a7
loklokyuen <loklokyuen.m@gmail.com>
2025-02-18 09:08:23 +0000

    style: add styling

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,7 +1,10 @@
 export default function Comment({ comment }) {
-    return <div className="">
+    const formattedDate = new Date(comment.created_at).toLocaleString();
+
+    return  <div className="border-t w-full border-highland-500 mt-1">
         <p className="font-semibold text-green-kelp-500 text-left">{comment.author}</p>
         <p className="text-green-kelp-800 text-left bg-shadow-green-300">{comment.body}</p>
+        <p className="text-right">Posted on {formattedDate}</p>
         <p className="text-left">
             <i className="fa-regular fa-thumbs-up"></i> <i className="fa-regular fa-thumbs-down"></i> {comment.votes}
         </p>
